61aa23b7554334880ce8d34193f946130a5c6aa6
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-04-22 21:28:09 +0200

    Added crud methods

---
+++ src/Controller/CustomersController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Customer;
+use App\Form\CustomerType;
+use App\Repository\CustomerRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class CustomersController extends AbstractController
+{
+    /**
+     * @Route("/customers", name="customers", methods={"GET"})
+     */
+    public function index(CustomerRepository $customerRepository): Response
+    {
+        return $this->render('customers/index.html.twig', [
+            'customers' => $customerRepository->findAll()
+        ]);
+    }
+
+    /**
+     * @Route("/customers/add", name="customers/add", methods={"GET","POST"})
+     * @param Request $request
+     * @return Response
+     */
+    public function add(Request $request): Response
+    {
+        $customer = new Customer();
+        $form = $this->createForm(CustomerType::class, $customer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($customer);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('customers');
+        }
+        return $this->render('customers/add.html.twig',[
+            'customer' => $customer,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/customers/edit/{id}", name="customers/edit", methods={"GET","POST"})
+     * @param Request $request
+     * @param Customer $customer
+     * @return Response
+     */
+    public function edit(Request $request, Customer $customer): Response
+    {
+        $form = $this->createForm(CustomerType::class, $customer);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            $this->getDoctrine()->getManager()->flush();
+            return $this->redirectToRoute('customers');
+        }
+
+        return $this->render('customers/edit.html.twig',[
+           'customer' => $customer,
+           'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/customers/delete/{id}", name="customers/delete", methods={"DELETE"})
+     * @param Request $request
+     * @param Customer $customer
+     * @return Response
+     */
+    public function delete(Request $request, Customer $customer): Response
+    {
+    if ($this->isCsrfTokenValid('delete'.$customer->getId(),$request->request->get('_token')))
+    {
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->remove($customer);
+        $entityManager->flush();
+    }
+
+    return $this->redirectToRoute('customers');
+    }
+
+
+}
