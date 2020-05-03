bd758a09ca8ca0e22b81512d0f6daaa2af753fe6
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-05-03 12:02:07 +0200

    Added Product model

---
+++ src/Entity/Product.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace App\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass="App\Repository\ProductRepository")
+ */
+class Product
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity="App\Entity\Category", inversedBy="products")
+     */
+    private $category_id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $product_name;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $unit_price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryId(): ?Category
+    {
+        return $this->category_id;
+    }
+
+    public function setCategoryId(?Category $category_id): self
+    {
+        $this->category_id = $category_id;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->product_name;
+    }
+
+    public function setProductName(string $product_name): self
+    {
+        $this->product_name = $product_name;
+
+        return $this;
+    }
+
+    public function getUnitPrice(): ?int
+    {
+        return $this->unit_price;
+    }
+
+    public function setUnitPrice(int $unit_price): self
+    {
+        $this->unit_price = $unit_price;
+
+        return $this;
+    }
+}
