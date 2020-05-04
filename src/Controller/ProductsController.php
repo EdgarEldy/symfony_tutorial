e5d0dca8760a6b9e094939814d1417fdcdbab860
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-05-04 12:06:53 +0200

    Updated crud methods

--- src/Controller/ProductsController.php
+++ src/Controller/ProductsController.php
@@ -3,6 +3,8 @@
 namespace App\Controller;
 
 use App\Entity\Product;
+use App\Form\ProductType;
+use App\Repository\CategoryRepository;
 use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -30,9 +32,31 @@ class ProductsController extends AbstractController
      * @Route("/products/add", name="products/add", methods={"GET","POST"})
      * @param Request $request
      */
-    public function add(Request $request): Response
+    public function add(Request $request, CategoryRepository $categoryRepository): Response
     {
+        $product = new Product();
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            //Flash message
+            $this->addFlash(
+                'product_saved',
+                'Product has been saved successfully'
+            );
 
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($product);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('products/add.html.twig',[
+            //'categories' => $categoryRepository->findAll(),
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
 
     }
 
@@ -44,7 +68,25 @@ class ProductsController extends AbstractController
      */
     public function edit(Request $request, Product $product): Response
     {
+        $form = $this->createForm(ProductType::class, $product);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid())
+        {
+            //Flash message
+            $this->addFlash(
+                'product_updated',
+                'Product has been updated successfully !'
+            );
 
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->flush();
+            return $this->redirectToRoute('products');
+        }
+
+        return $this->render('products/edit.html.twig', [
+            'product' => $product,
+            'form' => $form->createView()
+        ]);
     }
 
     /**
@@ -55,6 +97,19 @@ class ProductsController extends AbstractController
      */
     public function delete(Request $request, Product $product): Response
     {
+        if ($this->isCsrfTokenValid('delete' . $product->getId(), $request->request->get('_token')))
+        {
+            //Flash message
+            $this->addFlash(
+                'product_removed',
+                'Product has been removed successfully !'
+            );
 
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($product);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('products');
     }
 }
