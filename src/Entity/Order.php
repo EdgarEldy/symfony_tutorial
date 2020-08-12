2ba0f322415c54d0e5a6d47fa5f0e771a1e98266
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-08-12 23:36:52 +0200

    Added Order model

---
+++ src/Entity/Order.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="orders")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $customer_id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Product::class, inversedBy="orders")
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $product_id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $qty;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $total;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomerId(): ?Customer
+    {
+        return $this->customer_id;
+    }
+
+    public function setCustomerId(?Customer $customer_id): self
+    {
+        $this->customer_id = $customer_id;
+
+        return $this;
+    }
+
+    public function getProductId(): ?Product
+    {
+        return $this->product_id;
+    }
+
+    public function setProductId(?Product $product_id): self
+    {
+        $this->product_id = $product_id;
+
+        return $this;
+    }
+
+    public function getQty(): ?string
+    {
+        return $this->qty;
+    }
+
+    public function setQty(string $qty): self
+    {
+        $this->qty = $qty;
+
+        return $this;
+    }
+
+    public function getTotal(): ?string
+    {
+        return $this->total;
+    }
+
+    public function setTotal(string $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+}
