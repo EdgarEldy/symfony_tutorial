5a3139185b5f62ba7ab4f014d91e9a94b4e97dfd
Edgar MUHAMYANGABO <ed.eldy21@live.com>
2020-08-18 19:36:25 +0200

    Added a function for retrieving orders

--- src/Controller/OrdersController.php
+++ src/Controller/OrdersController.php
@@ -2,19 +2,31 @@
 
 namespace App\Controller;
 
+use App\Entity\Order;
+use App\Form\OrderType;
+use App\Repository\CustomerRepository;
+use App\Repository\OrderRepository;
+use App\Repository\ProductRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class OrdersController extends AbstractController
 {
     /**
-     * @Route("/orders", name="orders")
+     * @Route("/orders", name="orders", methods={"GET"})
+     * @param OrderRepository $orderRepository
+     * @return Response
      */
-    public function index()
+    public function index(OrderRepository $orderRepository): Response
     {
-        return $this->json([
-            'message' => 'Welcome to your new controller!',
-            'path' => 'src/Controller/OrdersController.php',
+        $orderRepository = $this->getDoctrine()
+            ->getRepository(Order::class)
+            ->findOrders();
+
+        return $this->render('orders/index.html.twig', [
+            'orders' => $orderRepository
         ]);
     }
 }
