a47627fa7c960076437d4d30e51daf4ab23d6f18
Edgar Eldy <ed.eldy21@live.com>
2022-12-20 17:01:24 +0200

    Add Order Entity

---
+++ src/Entity/Order.php
@@ -0,0 +1,112 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\OrderRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=OrderRepository::class)
+ * @ORM\Table(name="`order`")
+ */
+class Order
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Customer::class, inversedBy="orders")
+     */
+    private $customer_id;
+
+    /**
+     * @ORM\ManyToMany(targetEntity=Product::class, inversedBy="orders")
+     */
+    private $product_id;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $qty;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $total;
+
+    public function __construct()
+    {
+        $this->product_id = new ArrayCollection();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCustomerId(): ?Customer
+    {
+        return $this->customer_id;
+    }
+
+    public function setCustomerId(?Customer $customer_id): self
+    {
+        $this->customer_id = $customer_id;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Product>
+     */
+    public function getProductId(): Collection
+    {
+        return $this->product_id;
+    }
+
+    public function addProductId(Product $productId): self
+    {
+        if (!$this->product_id->contains($productId)) {
+            $this->product_id[] = $productId;
+        }
+
+        return $this;
+    }
+
+    public function removeProductId(Product $productId): self
+    {
+        $this->product_id->removeElement($productId);
+
+        return $this;
+    }
+
+    public function getQty(): ?int
+    {
+        return $this->qty;
+    }
+
+    public function setQty(int $qty): self
+    {
+        $this->qty = $qty;
+
+        return $this;
+    }
+
+    public function getTotal(): ?float
+    {
+        return $this->total;
+    }
+
+    public function setTotal(float $total): self
+    {
+        $this->total = $total;
+
+        return $this;
+    }
+}
