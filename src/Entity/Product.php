f7eb9dc03c140ee36afae0f11e0851c9663e5328
Edgar Eldy <ed.eldy21@live.com>
2022-12-20 16:28:42 +0200

    Add Product Entity with relationship to Category

---
+++ src/Entity/Product.php
@@ -0,0 +1,75 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ProductRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=ProductRepository::class)
+ */
+class Product
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\ManyToOne(targetEntity=Category::class, inversedBy="products")
+     */
+    private $category_id;
+
+    /**
+     * @ORM\Column(type="string", length=45)
+     */
+    private $product_name;
+
+    /**
+     * @ORM\Column(type="float")
+     */
+    private $unit_price;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getCategoryId(): ?Category
+    {
+        return $this->category_id;
+    }
+
+    public function setCategoryId(?Category $category_id): self
+    {
+        $this->category_id = $category_id;
+
+        return $this;
+    }
+
+    public function getProductName(): ?string
+    {
+        return $this->product_name;
+    }
+
+    public function setProductName(string $product_name): self
+    {
+        $this->product_name = $product_name;
+
+        return $this;
+    }
+
+    public function getUnitPrice(): ?float
+    {
+        return $this->unit_price;
+    }
+
+    public function setUnitPrice(float $unit_price): self
+    {
+        $this->unit_price = $unit_price;
+
+        return $this;
+    }
+}
