3bf36a58e39085c4a9e25df7b50233079570c8c6
Edgar Eldy <ed.eldy21@live.com>
2022-12-21 10:18:03 +0200

    uses the Category.category_name property as the visible option string

--- src/Form/ProductType.php
+++ src/Form/ProductType.php
@@ -2,7 +2,9 @@
 
 namespace App\Form;
 
+use App\Entity\Category;
 use App\Entity\Product;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
@@ -12,9 +14,15 @@ class ProductType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
+            ->add('category', Entity::class, [
+                // looks for choices from this entity
+                'class' => Category::class,
+
+                // uses the Category.category_name property as the visible option string
+                'choice_label' => 'category_name',
+            ])
             ->add('product_name')
             ->add('unit_price')
-            ->add('category_id')
             ->add('orders')
         ;
     }
